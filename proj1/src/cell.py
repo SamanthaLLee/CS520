dea1130462ba21996b94f5d1b8ba0e1bfce0eb71
SamanthaLLee <samantha.lin.lee@gmail.com>
2021-09-23 12:15:01 -0400

    Reformatted and commented

--- proj1/src/cell.py
+++ proj1/src/cell.py
@@ -1,4 +1,31 @@
 class Cell:
+    """
+    A class used to represent a Cell in the gridworld
+
+    ...
+
+    Attributes
+    ----------
+    x : int
+        the x-coordinate of the cell
+    y : int
+        the y-coordinate of the cell
+    g : float
+         the length of the shortest path discovered from the initial search point to cell n so far
+    h : float
+        the heuristic value, estimating the distance from the cell n to the goal node
+    f : float
+        an estimate of the distance from the initial search node to the final goal node through cell n (g+h)
+    blocked : boolean
+        whether the cell is actually blocked in the gridworld
+    seen : boolean
+        whether the agent has seen that the current cell is blocked
+    parent : Cell
+        a pointer to the previous node along the shortest path to n
+    child : Cell
+        a pointer to the next node along the shortest path to n
+    """
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -6,6 +33,6 @@ class Cell:
         self.h = float('inf')
         self.f = float('inf')
         self.blocked = False
-        self.seen = False
+        self.seen = False  # we need to clarify our understandings of this value
         self.parent = None
         self.child = None
--- proj1/src/main.py
+++ proj1/src/main.py
@@ -2,18 +2,20 @@ from array import *
 from queue import PriorityQueue
 import numpy as np
 import random
+from cell import Cell
 
-from .cell import Cell
-
-# remove globals
+# Global gridworld of Cell objects
 gridworld = []
+
+# Global goal cell
 goal = []
 
 
-def generateGridworld(dim, p):
+def generategridworld(dim, p):
     """Generates a random gridworld based on user inputs"""
     global goal, gridworld
 
+    # Cells are constructed in the following way:
     # Cell(g, h, f, blocked, seen, parent)
     gridworld = [[Cell(x, y) for x in range(dim)] for y in range(dim)]
 
@@ -24,39 +26,52 @@ def generateGridworld(dim, p):
             if rand < p:
                 gridworld[i][j].blocked = 1
 
-    # Exclude the upper left corner(chosen to be the start position) and
-    # the lower right corner(chosen to be the end position) from being blocked.
-    # Initialize starting cell values
-    gridworld[0][0].g = 0
-    gridworld[0][0].h = getHeuristic(0, 0)
-    gridworld[0][0].f = gridworld[0][0].g + gridworld[0][0].h
-    gridworld[0][0].seen = True
+    # Set the goal node
+    goal = [dim-1, dim-1]
 
+    # Ensure that the start and end positions are unblocked
     gridworld[0][0].blocked = 0
     gridworld[dim-1][dim-1].blocked = 0
-    goal = [dim-1, dim-1]
+
+    # Initialize starting cell values
+    gridworld[0][0].g = 0
+    gridworld[0][0].h = getheuristic(0, 0)
+    gridworld[0][0].f = gridworld[0][0].g + gridworld[0][0].h
+    gridworld[0][0].seen = True
 
     print(np.matrix(gridworld))
 
 
-def AStar(start):
+def astar(start):
+    """Performs the A* algorithm on the gridworld
+
+    Args:
+        start (Cell): The cell from which AStar will find a path to the goal
+
+    Returns:
+        Cell: The first cell in the shortest path and the head of a linked list representing the shortest path
+    """
     global goal, gridworld
-    g = 0  # Length of the shortest path
     fringe = PriorityQueue()
 
+    # Vectors that represent the four cardinal directions
     directions = [(1, 0),
                   (-1, 0),
                   (0, 1),
                   (0, -1)]
 
-    path = Cell(-1, -1)  # Dummy
+    # Dummy cell to anchor final path
+    path = Cell(-1, -1)
+
+    # Pointer to move along path
     ptr = path
 
     # Add start to fringe
     curr = start
     fringe.put(curr.f, curr)
 
-    # Generate all children and add to fringe
+    # Generate all valid children and add to fringe
+    # Terminate loop if fringe is empty or if path has reached goal
     while not fringe.isEmpty() or curr != goal:
         curr = fringe.get()
 
@@ -64,18 +79,20 @@ def AStar(start):
             xx = curr.x + x
             yy = curr.y + y
             nextCell = gridworld[xx][yy]
-            # Add neighbors to gridworld if inbounds, not blocked, and unseen
-            if isInBounds([xx,yy]) and not nextCell.blocked:
+
+            # Add children to gridworld if inbounds, not blocked, and unseen
+            # double check this. we need to use the current knowledge of the agent.
+            if isinbounds([xx, yy]) and not nextCell.blocked:
                 # don't add to fringe i think? not sure
                 if nextCell.seen:
-
-                # compare f value to one in fringe, update if nextCell's < cell in fringe
+                    print("do thing")
+                    # compare f value to one in fringe, update if nextCell's < cell in fringe
                 elif nextCell in fringe:
-                    
+                    print("do thing")
                 else:
-                # Update cell info
+                    # Update cell info
                     nextCell.g = curr.g + 1
-                    nextCell.h = getHeuristic(x, y)
+                    nextCell.h = getheuristic(x, y)
                     nextCell.f = nextCell.g + nextCell.h
                     fringe.put(nextCell.f, nextCell)
 
@@ -88,23 +105,29 @@ def AStar(start):
 
 
 def solve():
+    """
+    Solves the gridworld using Repeated Forward A*.
+    """
     global goal, gridworld
-    print("do thing")
 
-    curr = AStar(gridworld[0, 0])
+    path = astar(gridworld[0, 0])
+    curr = path
 
     while(True):
         if(curr.child is None):
-            # goal found
+            # goal found, return path
             break
 
         # Run into blocked cell
         if curr.blocked == True:
             curr.seen == True
-            curr = AStar(curr.parent)
+            path = astar(curr.parent)
+            curr = path
             continue
 
         curr = curr.child
+
+    return path
 
     # plan shortest presumed path from its current position to the goal.
     # attempt to follow this path plan, observing cells in its field of view as it moves
@@ -115,23 +138,33 @@ def solve():
     #     gridworld[x][y].seen == True
 
 
-def isInBounds(curr):
-    global gridworld
+def isinbounds(curr):
     """Determines whether next move is within bounds"""
+    global gridworld
     return 0 <= curr[0] < len(gridworld) and 0 <= curr[1] < len(gridworld[0])
 
 
-def getHeuristic(x, y):
-    global goal
+def getheuristic(x, y):
     """Calculates Manhattan distance"""
+    global goal
     return abs(x-goal[0]) + abs(y-goal[1])
 
 
+def isfloat(str):
+    """Determines whether a given string can be converted to float"""
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
+
 if __name__ == "__main__":
-    # prompt user to enter dimensions
     dim = input("What is the length of your gridworld? ")
+    while not dim.isdigit() or int(dim) < 0:
+        dim = input("Enter a valid length. ")
     p = input("With what probability will a cell be blocked? ")
-    while float(p) > 1 or float(p) < 0:
+    while not isfloat(p) or float(p) > 1 or float(p) < 0:
         p = input("Enter a valid probability. ")
-    generateGridworld(int(dim), float(p))
+    generategridworld(int(dim), float(p))
     solve()

7c79c8e09d3cb02b1c5b371b287d3ba8aa93cbb9
SamanthaLLee <samantha.lin.lee@gmail.com>
2021-12-13 17:24:05 -0500

    check if solvable maze during generation

--- proj4/src/solve.py
+++ proj4/src/solve.py
@@ -4,6 +4,8 @@ from queue import PriorityQueue
 from cell import Cell
 import itertools
 import time
+import numpy as np
+
 
 # Global gridworld of Cell objects
 gridworld = []
@@ -19,53 +21,64 @@ alldirections = [(1, 0), (-1, 0), (0, 1), (0, -1),
 numcellsprocessed = 0
 trajectorylen = 0
 dim = 0
-finaldiscovered = False
+# finaldiscovered = False
 fullgridworld = False
 
 totalplanningtime = 0
 
-equation_KB = set()
+input_states = []  # [np.zeros(shape=(5, 5, 10))]  # 10 states of 10x10 grids
+current_locations = []  # np.zeros(shape=(2, 100))  # 100 coordinates
+output_states = []  # np.arange(10)  # 10 actions
 
 
-def generategridworld(d, p):
+def generategridworld(d):
     """Generates a random gridworld based on user inputs"""
     global goal, gridworld, dim
     dim = d
+    p = .3
     # Cells are constructed in the following way:
     # Cell(g, h, f, blocked, seen, parent)
-    gridworld = [[Cell(x, y) for y in range(dim)] for x in range(dim)]
-    id = 0
 
-    # Let each cell independently be blocked with probability p, and empty with probability 1−p.
-    for i in range(dim):
-        for j in range(dim):
-            curr = gridworld[i][j]
+    while(True):
+        gridworld = [[Cell(x, y) for y in range(dim)] for x in range(dim)]
+        id = 0
 
-            # Determined block status
-            rand = random.random()
-            if rand < p:
-                curr.blocked = 1
+        # Let each cell independently be blocked with probability p, and empty with probability 1−p.
+        for i in range(dim):
+            for j in range(dim):
+                curr = gridworld[i][j]
 
-            # Assign ID
-            curr.id = id
-            id += 1
+                # Determined block status
+                rand = random.random()
+                if rand < p:
+                    curr.blocked = 1
 
-            # Set N and H
-            curr.N = get_num_neighbors(i, j)
-            curr.H = curr.N
+                # Assign ID
+                curr.id = id
+                id += 1
 
-    # Set the goal node
-    goal = gridworld[dim-1][dim-1]
+                # Set N and H
+                curr.N = get_num_neighbors(i, j)
+                curr.H = curr.N
 
-    # Ensure that the start and end positions are unblocked
-    gridworld[0][0].blocked = 0
-    goal.blocked = 0
+        # Set the goal node
+        goal = gridworld[dim-1][dim-1]
 
-    # Initialize starting cell values
-    gridworld[0][0].g = 1
-    gridworld[0][0].h = get_weighted_manhattan_distance(0, 0, goal.x, goal.y)
-    gridworld[0][0].f = gridworld[0][0].g + gridworld[0][0].h
-    gridworld[0][0].seen = True
+        # Ensure that the start and end positions are unblocked
+        gridworld[0][0].blocked = 0
+        goal.blocked = 0
+
+        # Initialize starting cell values
+        gridworld[0][0].g = 1
+        gridworld[0][0].h = get_weighted_manhattan_distance(
+            0, 0, goal.x, goal.y)
+        gridworld[0][0].f = gridworld[0][0].g + gridworld[0][0].h
+        gridworld[0][0].seen = True
+
+        result, len = astar(gridworld[0][0], 0)
+
+        if result is not None:
+            break
 
 
 def printGridworld():
@@ -106,7 +119,7 @@ def astar(start, agent):
         Cell: The head of a Cell linked list containing the shortest path
         int: Length of returned final path
     """
-    global goal, gridworld, finaldiscovered, fullgridworld, cardinaldirections, numcellsprocessed, totalplanningtime
+    global goal, gridworld, fullgridworld, cardinaldirections, numcellsprocessed, totalplanningtime
     starttime = time.time()
     fringe = PriorityQueue()
     fringeSet = set()
@@ -136,21 +149,23 @@ def astar(start, agent):
 
             if is_in_bounds([xx, yy]):
                 nextCell = gridworld[xx][yy]
-                validchild = ((agent == 1 or agent == 2) and not (nextCell.blocked and nextCell.seen)) or (
-                    (agent == 3 or agent == 4) and not (nextCell.blocked and nextCell.confirmed))
+                validchild = ((agent == 1) and not (nextCell.blocked and nextCell.seen)) or (
+                    (agent == 2) and not (nextCell.blocked and nextCell.confirmed)) or (
+                        (agent == 0) and not nextCell.blocked)
+
                 # Add children to fringe if inbounds AND unblocked and unseen
-                if (finaldiscovered and nextCell.seen) or not finaldiscovered:
-                    if validchild or (fullgridworld and not nextCell.blocked):
-                        # Add child if not already in fringe
-                        # If in fringe, update child in fringe if old g value > new g value
-                        if(((not nextCell.id in fringeSet) or (nextCell.g > curr.g + 1)) and nextCell.id not in seenSet):
-                            nextCell.parent = curr
-                            nextCell.g = curr.g + 1
-                            nextCell.h = get_weighted_manhattan_distance(
-                                xx, yy, goal.x, goal.y)
-                            nextCell.f = nextCell.g + nextCell.h
-                            fringe.put((nextCell.f, nextCell))
-                            fringeSet.add(nextCell.id)
+                # if nextCell.seen:
+                if validchild:
+                    # Add child if not already in fringe
+                    # If in fringe, update child in fringe if old g value > new g value
+                    if(((not nextCell.id in fringeSet) or (nextCell.g > curr.g + 1)) and nextCell.id not in seenSet):
+                        nextCell.parent = curr
+                        nextCell.g = curr.g + 1
+                        nextCell.h = get_weighted_manhattan_distance(
+                            xx, yy, goal.x, goal.y)
+                        nextCell.f = nextCell.g + nextCell.h
+                        fringe.put((nextCell.f, nextCell))
+                        fringeSet.add(nextCell.id)
 
     # Return None if no solution exists
     if len(fringeSet) == 0:
@@ -180,63 +195,18 @@ def astar(start, agent):
         return None, 0
 
 
-def solve0():
-    """
-    Solves the gridworld using Repeated Forward A*.
-    """
-    global goal, gridworld, cardinaldirections, trajectorylen
-    agent = 0
-    path, len = astar(gridworld[0][0], agent)
-
-    # Initial A* failed - unsolvable gridworld
-    if path is None:
-        return None
-
-    # Attempt to move along planned path
-    curr = path
-    while(True):
-        # A* failed - unsolvable gridworld
-        if(curr is None):
-            return None
-
-        # Pre-process cell
-        curr.seen = True
-        trajectorylen += 1
-
-        # Goal found
-        if(curr.child is None):
-            return path
-
-        # Run into blocked cell
-        if curr.blocked == True:
-            trajectorylen -= 1
-            path, len = astar(curr.parent, agent)
-            curr = path
-
-        # Continue along A* path
-        else:
-            # Take note of environment within viewing distance (adjacent cells)
-            for dx, dy in cardinaldirections:
-                xx, yy = curr.x + dx, curr.y + dy
-
-                # Only mark blocked neighbors as seen
-                if is_in_bounds([xx, yy]) and gridworld[xx][yy].blocked:
-                    neighbor = gridworld[xx][yy]
-                    neighbor.seen = True
-            # Mark current cell as seen and move onto next cell along A* path
-            curr = curr.child
-
-
 def solve1():
     """
-    Agent 1 - The Blindfolded Agent: bumps into walls
+    Agent 1 - 4-Neighbor Agent: Can see all 4 cardinal neighbors at once
     """
-    global gridworld, cardinaldirections, trajectorylen
+    global gridworld, cardinaldirections, trajectorylen, dim
 
     agent = 1
 
     path, len = astar(gridworld[0][0], agent)
 
+    currstate = np.full((dim, dim), 2)
+
     # Initial A* failed - unsolvable gridworld
     if path is None:
         return None
@@ -244,6 +214,7 @@ def solve1():
     # Attempt to move along planned path
     curr = path
     while True:
+
         # A* failed - unsolvable gridworld
         if curr is None:
             return None
@@ -256,74 +227,42 @@ def solve1():
         if curr.child is None:
             return path
 
-        # Run into blocked cell
-        if curr.blocked == True:
-            trajectorylen -= 1
-            path, len = astar(curr.parent, agent)
-            curr = path
-
-        # Continue along A* path
-        else:
-            # Move onto next cell along A* path
-            curr = curr.child
-
-
-def solve2():
-    """
-    Agent 2 - 4-Neighbor Agent: Can see all 4 cardinal neighbors at once
-    """
-    global gridworld, cardinaldirections, trajectorylen
-
-    agent = 2
-
-    path, len = astar(gridworld[0][0], agent)
-
-    # Initial A* failed - unsolvable gridworld
-    if path is None:
-        return None
-
-    # Attempt to move along planned path
-    curr = path
-    while True:
-        # A* failed - unsolvable gridworld
-        if curr is None:
-            return None
-
-        # Pre-process cell
-        curr.seen = True
-        trajectorylen += 1
-
-        # Goal found
-        if curr.child is None:
-            return path
+        current_locations.append([curr.x, curr.y])
 
         # Run into blocked cell
         if curr.blocked == True:
+            currstate[curr.x][curr.y] = 1
             trajectorylen -= 1
+            output_states.append(get_action(curr, curr.parent))
             path, len = astar(curr.parent, agent)
             curr = path
 
         # Continue along A* path
         else:
             # Take note of environment within viewing distance (adjacent cells)
+            currstate[curr.x][curr.y] = 0
             for dx, dy in cardinaldirections:
                 xx, yy = curr.x + dx, curr.y + dy
 
                 # Only mark blocked neighbors as seen
                 if is_in_bounds([xx, yy]) and gridworld[xx][yy].blocked:
                     neighbor = gridworld[xx][yy]
+                    currstate[neighbor.x][neighbor.y] = 1
                     neighbor.seen = True
             # Move onto next cell along A* path
+            output_states.append(get_action(curr, curr.child))
             curr = curr.child
 
+        input_states.append(currstate)
 
-def solve3():
+
+def solve2():
     """
-    Agent 3 - Example Inference Agent
+    Agent 2 - Example Inference Agent
     """
     global goal, gridworld, alldirections, trajectorylen
 
-    agent = 3
+    agent = 2
 
     path, len = astar(gridworld[0][0], agent)
 
@@ -347,7 +286,7 @@ def solve3():
             return path
 
         # Update neighbors and cascade inferences
-        updatekb3(curr)
+        updatekb(curr)
 
         # Replan if agent has run into blocked cell
         if curr.blocked == True:
@@ -356,9 +295,9 @@ def solve3():
             continue
         else:
             # Sense number of blocked and confirmed neighbors for curr
-            senseorcount3(curr, True)
+            senseorcount(curr, True)
             # Make inferences from this sensing
-            infer3(curr)
+            infer(curr)
 
             # Replan if agent finds inferred block in path
             ptr = curr.child
@@ -376,7 +315,7 @@ def solve3():
                 curr = curr.child
 
 
-def senseorcount3(curr, sense):
+def senseorcount(curr, sense):
     """Sets curr's C, E, H, B values based on current KB
     Args:
         curr (cell): current cell
@@ -409,7 +348,7 @@ def senseorcount3(curr, sense):
                 curr.H -= 1
 
 
-def infer3(curr):
+def infer(curr):
     """Tests for the 3 given inferences
     Args:
         curr (cell): current cell to make inferences on
@@ -443,21 +382,21 @@ def infer3(curr):
         return inferencemade
 
 
-def updatekb3(curr):
+def updatekb(curr):
     for x, y in alldirections:
         xx = curr.x + x
         yy = curr.y + y
         if is_in_bounds([xx, yy]):
             # Find all inbounds neighbors of curr
             neighbor = gridworld[xx][yy]
-            senseorcount3(neighbor, False)
+            senseorcount(neighbor, False)
             if neighbor.seen and neighbor.blocked == 0 and neighbor.H > 0:
-                if infer3(neighbor):
+                if infer(neighbor):
                     for x, y in alldirections:
                         xx2 = neighbor.x + x
                         yy2 = neighbor.y + y
                         if is_in_bounds([xx2, yy2]):
-                            updatekb3(gridworld[xx2][yy2])
+                            updatekb(gridworld[xx2][yy2])
 
 
 def is_in_bounds(curr):
@@ -479,3 +418,14 @@ def get_weighted_manhattan_distance(x1, y1, x2, y2):
     """Manhattan: d((x1, y1),(x2, y2)) = abs(x1 - x2) + abs(y1 - y2)"""
 
     return 2*(abs(x1-x2) + abs(y1-y2))
+
+
+def get_action(curr, next):
+    if curr.y < next.y:
+        return 3  # right
+    elif curr.y > next.y:
+        return 2  # left
+    elif curr.x < next.x:
+        return 1  # down
+    else:
+        return 0  # up
